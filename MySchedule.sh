4f44f5c7651b73251d9ccaebc3d4f17c94c80111
kalen jackson <kalen.jackson@hgs.hiddengeniusproject.org>
2018-10-16 18:44:46 -0700

    first commit

---
+++ MySchedule.sh
@@ -0,0 +1,29 @@
+cd ~/Desktop
+mkdir week2
+cd week2
+touch index.html
+mkdir Kalen Chris
+cd Kalen
+mkdir Mondayk Tuesdayk Wednesdayk Thursdayk Fridayk 
+cd ../Chris
+mkdir Mondayc Tuesdayc Wednesdayc Thursdayc Fridayc
+cd ../Kalen/Mondayk
+touch English.Mondayk Geometry.Mondayk DigitalMediaArts.Mondayk Biology.Mondayk Algebra.Mondayk
+cd ../Tuesdayk
+touch English.Tuesdayk Geometry.Tuesdayk DigitalMediaArts.Tuesdayk Biology.Tuesdayk Algebra.Tuesdayk
+cd ../Wednesdayk
+touch English.Wednesdayk Geometry.Wednesdayk DigitalMediaArts.Wednesdayk Biology.Wednesdayk Algebra.Wednesdayk
+cd ../Thursdayk
+touch English.Thursdayk Geometry.Thursdayk DigitalMediaArts.Thursdayk Biology.Thursdayk Algebra.Thursdayk
+cd ../Fridayk
+touch English.Fridayk Geometry.Fridayk DigitalMediaArts.Fridayk Biology.Fridayk Algebra.Fridayk
+cd ../../Chris/Mondayc
+touch English.Mondayc Biology.Mondayc PE.Mondayc Journalism.Mondayc Spanish.Mondayc Arm.Mondayc 
+cd ../Tuesdayc
+touch English.Tuesdayc PE.Tuesdayc Spanish.Tuesdayc Geometry.Tuesdayc
+cd ../Wednesdayc 
+touch Biology.Wednesdayc Journalism.Wednesdayc Arm.Wednesdayc
+cd ../Thursdayc
+touch English.Thursdayc PE.Thursdayc Spanish.Thursdayc Geometry.Thursdayc
+cd ../Fridayc
+touch Biology.Fridayc Journalism.Fridayc Arm.Fridayc SA.Fridayc
